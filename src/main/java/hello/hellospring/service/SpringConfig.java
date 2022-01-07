7c8c205a1075d87d614b70077f9c3ed56205da12
KimWonhee626 <kwh7739@gmail.com>
2022-01-07 14:14:02 +0900

    Modify SpringConfig.java - 자바 코드로 직접 스프링 빈 등록.

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// 수동으로 SpringBean에 등록
 @Configuration
 public class SpringConfig {
 
@@ -20,4 +21,5 @@ public class SpringConfig {
 
     // 1. memberService, memberRepository 를 SpringBean에 등록함
     // 2. 등록된 memberRepository를 MemberService에 넣어줌
+    // => 데이터베이스 연결할 때 return값만 변경하면 되기때문에 수정이 용이함
 }
