b6e08955c0aed735108c1560625b53fb224b9c1a
KimWonhee626 <kwh7739@gmail.com>
2022-01-12 16:21:19 +0900

    db연결 통합테스트 구현

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import com.sun.jdi.event.ExceptionEvent;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
@@ -15,8 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 // 스프링 테스트때 필요한 어노테이션
 @SpringBootTest
-// @Transactional
-
+@Transactional // test 끝나면 롤백해줌(메서드마다 적용됨)
 class MemberServiceIntegrationTest {
 
     @Autowired MemberService memberService;
@@ -26,7 +26,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         // given (무슨 데이터 기반?)
         Member member = new Member();
-        member.setName("wonhee");
+        member.setName("wonhee3");
 
         // when (뭘 검증하는지)
         Long saveId = memberService.join(member);
@@ -37,7 +37,7 @@ class MemberServiceIntegrationTest {
     }
 
     @Test
-    public void 중복회원_예외(){
+    public void 중복회원_예외() throws Exception {
         // given
         Member member1 = new Member();
         member1.setName("wonhee");
@@ -47,29 +47,13 @@ class MemberServiceIntegrationTest {
 
         // when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, ()
-        -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
 
         // then
         // 중복회원 있을 때
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
-        /*       try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e){ // 예외 발생(ㅈ
-            Assertions.assertThat(e.getMessage())
-                    .isEqualTo("이미 존재하는 회원입니다.");
-        }
-*/
     }
 
-    @Test
-    void findMember() {
-
-    }
-
-    @Test
-    void findOne() {
-    }
 }
